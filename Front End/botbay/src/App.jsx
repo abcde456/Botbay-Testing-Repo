eee6c6f6e351a98a7bdb78f73b9e15be132672e4
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-28 21:51:05 -0500

    Switch to hashrouter

--- Front End/botbay/src/App.jsx
+++ Front End/botbay/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; 
 import LandingPage from './assets/pages/landingpage.jsx';
 import SignUpPage from './assets/pages/signuppage.jsx'
 import SignInPage from './assets/pages/signinpage.jsx'
@@ -8,7 +8,7 @@ import Dashboard from './assets/pages/dashboard.jsx';
 
 function App() {
   return (
-    <Router basename="/Botbay-Testing-Repo"> 
+    <Router> 
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
